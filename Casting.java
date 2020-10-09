3e43dd423852c32a5656dc89ec5381e32f3bc078
cchsMartin <54595803+cchsMartin@users.noreply.github.com>
2020-10-09 13:01:19 -0400

    Add files via upload

---
+++ Casting.java
@@ -0,0 +1,10 @@
+
+public class Casting {
+
+	public static void main(String[] args) {
+		int hours = 25;
+		int	rate = 10 ;
+		System.out.println("hours / rate = " +  (double) (hours / rate)); 
+	}
+
+}
